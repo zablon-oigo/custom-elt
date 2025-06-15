91573034cf42ddeae31bf970586e103aa1c79513
Zablon <143833326+zablon-oigo@users.noreply.github.com>
2025-06-15 22:27:53 +0300

    add args

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
       retries = 0
     while retries < max_retries:
         try:
-            result = subprocess.run()
+            result = subprocess.run(
+                  ["pg_isready", "-h", host], check=True, capture_output=True, text=True
+            )
